10d40abf6383a2b0d56a50479e67572bcf4cddf7
yusuf-ishaku <kingshakzy@gmail.com>
2023-10-19 22:17:38 +0100

    First checkpoint

---
+++ index.js
@@ -0,0 +1,30 @@
+const {parse} = require('csv-parse');
+const fs = require('fs');
+const habitablePlanets = [];
+
+const upperLimit = 1.11;
+const lowerLimit = 0.66;
+
+let isHabitable = (obj) =>{
+    return obj['koi_disposition'] === 'CONFIRMED'
+        && obj['koi_insol'] > 0.36 && obj['koi_insol'] < 1.11
+        && obj['koi_prad'] < 1.6;
+}
+
+fs.createReadStream("./003 kepler-data.csv")
+.pipe(parse({
+    comment: "#",
+    columns: true
+}))
+.on('data', (data) =>{
+    if(isHabitable(data)){
+        habitablePlanets.push(data) 
+    }
+    
+})
+.on('error', (error) => console.log(error))
+.on('end', () =>{
+    console.log(habitablePlanets)
+    console.log(`${habitablePlanets.length} is the number of habitable planets`)
+})
+// parse()
